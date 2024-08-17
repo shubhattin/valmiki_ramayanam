ec946ad3f00a84e901432a1e1b96a6eb2628ade3
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-08-17 08:43:41 +0530

    added and finalized header

--- src/components/MainAppBar.svelte
+++ src/components/MainAppBar.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import { AppBar, popup } from '@skeletonlabs/skeleton';
 	import ThemeChanger from './ThemeChanger.svelte';
-	import { BiArrowBack, BiBookOpen } from 'svelte-icons-pack/bi';
+	import { BiArrowBack, BiBookOpen, BiLogoGithub } from 'svelte-icons-pack/bi';
 	import Icon from '@tools/Icon.svelte';
+	import { SiGithub } from 'svelte-icons-pack/si';
 	import { SiConvertio } from './icons';
 
 	export let page: 'home' | 'convert' | 'ramayan';
@@ -37,41 +38,69 @@
 	</svelte:fragment>
 	<svelte:fragment slot="trail">
 		<slot name="end"></slot>
-		{#if page !== 'ramayan'}
+		<div class="space-x-2">
 			<a
 				class="text-xl"
-				href="/ramayan"
+				href="https://github.com/shubhattin/sanskrit_text_transliteration"
+				target="_blank"
+				rel="noopener noreferrer"
 				use:popup={{
 					event: 'hover',
-					target: 'ramayan_popup',
+					target: 'github_popup',
 					placement: 'bottom'
 				}}
 			>
-				<span class="text-sm">रामायणम्</span>
-				<Icon src={BiBookOpen} class="text-2xl hover:fill-red-700 dark:hover:fill-sky-500" />
-				<div data-popup="ramayan_popup" class="variant-ghost-tertiary px-1 text-base">
-					श्रीमद्रामायणम्
+				<Icon
+					src={SiGithub}
+					class="-mt-1 mr-1 text-2xl hover:fill-indigo-700 dark:hover:fill-zinc-400"
+				/>
+				<div data-popup="github_popup" class="variant-ghost-tertiary px-1 text-base">
+					Project's Github Page
 					<div class="bg-surface-100-800-token arrow" />
 				</div>
 			</a>
-		{/if}
-		{#if page !== 'convert'}
-			<a
-				class="text-xl"
-				href="/convert"
-				use:popup={{
-					event: 'hover',
-					target: 'convert_popup',
-					placement: 'bottom'
-				}}
-			>
-				<Icon src={SiConvertio} class="text-2xl hover:fill-red-700 dark:hover:fill-sky-500" />
-				<div data-popup="convert_popup" class="variant-ghost-tertiary px-1 text-base">
-					Lipi Parivartak
-					<div class="bg-surface-100-800-token arrow" />
-				</div>
-			</a>
-		{/if}
+			{#if page !== 'ramayan'}
+				<a
+					href="/ramayan"
+					use:popup={{
+						event: 'hover',
+						target: 'ramayan_popup',
+						placement: 'bottom'
+					}}
+					class="hover:text-yellow-800 dark:hover:text-slate-300"
+				>
+					<span class="text-sm">रामायणम्</span>
+					<Icon src={BiBookOpen} class="text-2xl" />
+					<div
+						data-popup="ramayan_popup"
+						class="variant-ghost-tertiary px-1 text-base text-black dark:text-white"
+					>
+						श्रीमद्रामायणम्
+						<div class="bg-surface-100-800-token arrow" />
+					</div>
+				</a>
+			{/if}
+			{#if page !== 'convert'}
+				<a
+					class="text-xl"
+					href="/convert"
+					use:popup={{
+						event: 'hover',
+						target: 'convert_popup',
+						placement: 'bottom'
+					}}
+				>
+					<Icon
+						src={SiConvertio}
+						class="text-2xl hover:fill-emerald-600 dark:hover:fill-zinc-400"
+					/>
+					<div data-popup="convert_popup" class="variant-ghost-tertiary px-1 text-base">
+						Lipi Parivartak
+						<div class="bg-surface-100-800-token arrow" />
+					</div>
+				</a>
+			{/if}
+		</div>
 		<ThemeChanger />
 	</svelte:fragment>
 </AppBar>
