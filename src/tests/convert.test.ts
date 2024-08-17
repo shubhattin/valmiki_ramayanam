b47a3c03de2a4c7866e78639a61d46213f3e02e9
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-08-17 13:03:43 +0530

    changed actions scipt

--- src/tests/convert.test.ts
+++ src/tests/convert.test.ts
@@ -1,13 +1,13 @@
 import { describe, expect, it } from 'vitest';
 import yaml from 'js-yaml';
-import * as fs from 'fs/promises';
+import * as fs from 'fs';
 import LipiLekhikA from '../tools/converter';
 
 const LANG_LIST = ['de', 'te', 'kn', 'ta', 'bn', 'or', 'gu', 'ml'];
 
 describe('Basic Conversions', () => {
 	it('convert from devanagari to other scripts', async () => {
-		const list = yaml.load(await fs.readFile('./src/tests/data/tests.yml', 'utf-8')) as Record<
+		const list = yaml.load(fs.readFileSync('./src/tests/data/tests.yml', 'utf-8')) as Record<
 			string,
 			string
 		>[];
@@ -24,5 +24,11 @@ describe('Basic Conversions', () => {
 				expect(out_data).toBe(expected);
 			}
 		}
+	});
+});
+
+describe('Test Custom Patches', () => {
+	it('From Devanagari to Normaal', async () => {
+		//
 	});
 });
