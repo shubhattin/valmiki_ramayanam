f5f0b3a42dfd310da04f3513adb7922da4f8d703
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-08-20 08:03:00 +0530

    Change App Structure (#16)
    * changes
    
    * added icon
    
    * update
    
    * update
    
    * made route changes
    
    * added menu btn
    
    * made final changes

--- src/components/MainAppBar.svelte
+++ src/components/MainAppBar.svelte
@@ -1,106 +1,97 @@
 <script lang="ts">
 	import { AppBar, popup } from '@skeletonlabs/skeleton';
+	import type { PopupSettings } from '@skeletonlabs/skeleton';
 	import ThemeChanger from './ThemeChanger.svelte';
-	import { BiArrowBack, BiBookOpen, BiLogoGithub } from 'svelte-icons-pack/bi';
+	import { BiArrowBack } from 'svelte-icons-pack/bi';
 	import Icon from '@tools/Icon.svelte';
 	import { SiGithub } from 'svelte-icons-pack/si';
 	import { SiConvertio } from './icons';
+	import { AiOutlineMenu } from 'svelte-icons-pack/ai';
+	import { RiDocumentFileExcel2Line } from 'svelte-icons-pack/ri';
 
-	export let page: 'home' | 'convert' | 'ramayan';
+	export let page: 'home' | 'convert' | 'excel_tool';
+
+	const app_menu_popup: PopupSettings = {
+		event: 'click',
+		target: 'app_bar_menu',
+		placement: 'left-end',
+		closeQuery: '.will-close'
+	};
 </script>
 
 <AppBar>
 	<svelte:fragment slot="lead">
 		<slot name="start" />
 		{#if page !== 'home'}
-			<a
-				class="text-xl"
-				href="/"
-				use:popup={{
-					event: 'hover',
-					target: 'home_popup',
-					placement: 'bottom'
-				}}
-			>
+			<a class="mr-2 text-xl" href="/" title="श्रीरामायणम्">
 				<Icon
 					src={BiArrowBack}
-					class="-mt-1 mr-1 text-2xl hover:fill-red-700 dark:hover:fill-sky-500"
+					class="-mt-1 mr-1 text-2xl hover:fill-blue-600 dark:hover:fill-sky-500"
 				/>
-				<div data-popup="home_popup" class="variant-ghost-tertiary px-1 text-base">
-					Home Page
-					<div class="bg-surface-100-800-token arrow" />
-				</div>
 			</a>
 		{/if}
-	</svelte:fragment>
-	<svelte:fragment slot="headline">
 		<slot name="headline"><span></span></slot>
 	</svelte:fragment>
+	<!-- <svelte:fragment slot="headline">
+		<slot name="headline"><span></span></slot>
+	</svelte:fragment> -->
 	<svelte:fragment slot="trail">
 		<slot name="end"></slot>
 		<div class="space-x-2">
-			<a
-				class="text-xl"
-				href="https://github.com/shubhattin/valmiki_ramayanam"
-				target="_blank"
-				rel="noopener noreferrer"
-				use:popup={{
-					event: 'hover',
-					target: 'github_popup',
-					placement: 'bottom'
-				}}
-			>
-				<Icon
-					src={SiGithub}
-					class="-mt-1 mr-1 text-2xl hover:fill-indigo-700 dark:hover:fill-zinc-400"
-				/>
-				<div data-popup="github_popup" class="variant-ghost-tertiary px-1 text-base">
-					Project's Github Page
-					<div class="bg-surface-100-800-token arrow" />
-				</div>
-			</a>
-			{#if page !== 'ramayan'}
-				<a
-					href="/ramayan"
-					use:popup={{
-						event: 'hover',
-						target: 'ramayan_popup',
-						placement: 'bottom'
-					}}
-					class="hover:text-yellow-800 dark:hover:text-slate-300"
-				>
-					<span class="text-sm">रामायणम्</span>
-					<Icon src={BiBookOpen} class="text-2xl" />
-					<div
-						data-popup="ramayan_popup"
-						class="variant-ghost-tertiary px-1 text-base text-black dark:text-white"
-					>
-						श्रीमद्रामायणम्
-						<div class="bg-surface-100-800-token arrow" />
-					</div>
+			{#if page !== 'convert'}
+				<a class="text-xl" href="/convert" title="Lipi Parivartak">
+					<Icon
+						src={SiConvertio}
+						class="text-2xl hover:fill-emerald-600 dark:hover:fill-zinc-400"
+					/>
 				</a>
 			{/if}
-			{#if page !== 'convert'}
+		</div>
+		<button class="btn m-0 p-0" use:popup={app_menu_popup} title="App Menu">
+			<Icon
+				src={AiOutlineMenu}
+				class="text-3xl hover:text-gray-500 active:text-blue-600 dark:hover:text-gray-400 dark:active:text-blue-400"
+			/>
+		</button>
+		<div class="card z-50 rounded-lg px-3 py-2 shadow-xl" data-popup="app_bar_menu">
+			<div class="space-y-2">
 				<a
-					class="text-xl"
 					href="/convert"
-					use:popup={{
-						event: 'hover',
-						target: 'convert_popup',
-						placement: 'bottom'
-					}}
+					class="will-close text-md flex space-x-2 rounded-md px-2 py-1 font-bold hover:bg-gray-200 dark:hover:bg-gray-700"
 				>
 					<Icon
 						src={SiConvertio}
 						class="text-2xl hover:fill-emerald-600 dark:hover:fill-zinc-400"
 					/>
-					<div data-popup="convert_popup" class="variant-ghost-tertiary px-1 text-base">
-						Lipi Parivartak
-						<div class="bg-surface-100-800-token arrow" />
-					</div>
+					<span>Lipi Parivartak</span>
 				</a>
-			{/if}
+				<a
+					href="/excel_tool"
+					class="will-close text-md flex space-x-1 rounded-md px-2 py-1 hover:bg-gray-200 dark:hover:bg-gray-700"
+				>
+					<Icon
+						src={RiDocumentFileExcel2Line}
+						class="-mt-1 text-xl text-green-600 dark:text-green-400"
+					/>
+					<span>Excel File Transliterator</span>
+				</a>
+				<a
+					href="https://github.com/shubhattin/valmiki_ramayanam"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="will-close text-md flex space-x-1 rounded-md px-2 py-1 hover:bg-gray-200 dark:hover:bg-gray-700"
+				>
+					<Icon
+						src={SiGithub}
+						class="-mt-1 mr-1 text-2xl hover:fill-indigo-700 dark:hover:fill-zinc-400"
+					/>
+					<span>Project's Github Page</span>
+				</a>
+				<div class="wont-close text-md flex space-x-3 rounded-md px-2 py-1">
+					<span class="mt-1">Set Theme</span>
+					<ThemeChanger />
+				</div>
+			</div>
 		</div>
-		<ThemeChanger />
 	</svelte:fragment>
 </AppBar>
