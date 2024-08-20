376d829b7ef92a09391606fd4b2b7d4881929d5e
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-08-20 18:27:57 +0530

    added copy button

--- src/tests/convert.test.ts
+++ src/tests/convert.test.ts
@@ -1,29 +1,7 @@
 import { describe, expect, it } from 'vitest';
-import yaml from 'js-yaml';
-import * as fs from 'fs';
 import { lipi_parivartak_async } from '../tools/converter';
 
-const LANG_LIST = ['de', 'te', 'kn', 'ta', 'bn', 'or', 'gu', 'ml'];
-
-describe('Basic Conversions', () => {
-	it('Convert from Devanagari to Other scripts', async () => {
-		const list = yaml.load(fs.readFileSync('./src/tests/data/tests.yml', 'utf-8')) as Record<
-			string,
-			string
-		>[];
-		for (let data of list) {
-			const main_data = data['de'];
-			for (let lang in data) {
-				if (lang === 'de') continue;
-				const expected = data[lang];
-				const out_data = await lipi_parivartak_async(main_data, 'de', lang);
-				expect(out_data).toBe(expected);
-			}
-		}
-	});
-});
-
-describe('Test Custom Patches', () => {
+describe('Test Custom Patches on Normal <-> Devangari Texts', () => {
 	const dev_to_normal_texts = [
 		['प्रपञ्च रपञ्च', 'prapancha rapancha'],
 		// remove .. and . tesr
@@ -58,6 +36,32 @@ describe('Test Custom Patches', () => {
 			const expected = data[0];
 			const out_data = await lipi_parivartak_async(normal_text, SOURCE_LANG, TARGET_LANG);
 			expect(out_data).toBe(expected);
+		}
+	});
+});
+
+describe('Test anunAsika', () => {
+	const LANGS_TO_TEST = ['Telugu', 'Kannada'];
+	const TESTS = [
+		['सर्वान्को', 'सर्वान्को'],
+		['भवान्चापि', 'भवान्चापि'],
+		['सर्वम्को', 'सर्वम्को'],
+		['भुजङ्ग', 'भुजंग'],
+		['विरिञ्चि', 'विरिंचि'],
+		['काण्डे', 'कांडे'],
+		['दन्तवर्धन', 'दंतवर्धन'],
+		['कम्पन', 'कंपन']
+	];
+
+	it('Test anunAsika', async () => {
+		for (let lang of LANGS_TO_TEST) {
+			const SOURCE_LANG = 'Sanskrit';
+			const TARGET_LANG = lang;
+			for (let data of TESTS) {
+				const out_data_1 = await lipi_parivartak_async(data[0], SOURCE_LANG, TARGET_LANG);
+				const out_data_2 = await lipi_parivartak_async(data[1], SOURCE_LANG, TARGET_LANG);
+				expect(out_data_1).toBe(out_data_2);
+			}
 		}
 	});
 });
