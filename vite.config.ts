37d0ab65263538e884358d59053b3764448ab5b7
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-09-05 19:45:11 +0530

    Fix GA partytown (#35)
    * init
    
    * fix
    
    * fixed
    
    * fix
    
    * fixed build

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,11 @@ import { join } from 'path';
 import { defineConfig } from 'vitest/config';
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { build_json_files } from './src/tools/vite_plugin_build_json_from_yaml';
-import { partytownVite } from '@builder.io/partytown/utils';
-import * as fs from 'fs';
 
 export default defineConfig({
   plugins: [
     sveltekit(),
     purgeCss(),
-    partytownVite({
-      dest: join(__dirname, 'static', '~partytown')
-    }),
     build_json_files(
       'data/ramayan/trans_en',
       'data/ramayan/trans_en/json',
@@ -23,20 +18,7 @@ export default defineConfig({
         }
         return new_data;
       }
-    ),
-    {
-      name: 'Add ~partytown to config.json',
-      async buildEnd() {
-        const file = JSON.parse(fs.readFileSync('.vercel/output/config.json', 'utf8'));
-        file.routes.push({
-          src: '/~partytown/.+',
-          headers: {
-            'cache-control': 'public, immutable, max-age=31536000'
-          }
-        });
-        fs.writeFileSync('.vercel/output/config.json', JSON.stringify(file, null, 4));
-      }
-    }
+    )
   ],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
