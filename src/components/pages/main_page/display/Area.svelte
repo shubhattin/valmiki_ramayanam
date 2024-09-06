ff9873d44a4dd9f025d839b679dad707276bcde6
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-09-06 18:49:15 +0530

    updated

--- src/components/pages/main_page/display/Area.svelte
+++ src/components/pages/main_page/display/Area.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
   import { lipi_parivartak_async } from '@tools/converter';
   import Icon from '@tools/Icon.svelte';
-  import { get_possibily_not_undefined, copy_text_to_clipboard } from '@tools/kry';
-  import { RiSystemAddLargeLine } from 'svelte-icons-pack/ri';
   import { writable, type Unsubscriber, type Writable } from 'svelte/store';
   import { fade, scale, slide } from 'svelte/transition';
   import { SlideToggle, getModalStore } from '@skeletonlabs/skeleton';
   import type { ModalSettings } from '@skeletonlabs/skeleton';
   import { FiSave } from 'svelte-icons-pack/fi';
-  import { BsClipboard2Check, BsKeyboard } from 'svelte-icons-pack/bs';
+  import { BsKeyboard } from 'svelte-icons-pack/bs';
   import { cl_join } from '@tools/cl_join';
   import LipiLekhikA from '@tools/converter';
   import { client_raw } from '@api/client';
@@ -26,6 +24,7 @@
   import Modal from '@components/Modal.svelte';
   import { CgClose } from 'svelte-icons-pack/cg';
   import { ALL_LANG_SCRIPT_LIST } from '@tools/lang_list';
+  import TextDisplay from './TextDisplay.svelte';
 
   const query_client = useQueryClient();
   const modal_store = getModalStore();
@@ -73,15 +72,8 @@
       return data_map;
     }
   });
-  async function update_trans_data(index: number, text: string) {
-    const new_data = new Map($trans_lang_data.data);
-    new_data.set(index, text);
-    await query_client.setQueryData(trans_lang_data_query_key, new_data);
-  }
 
   let edit_language_typer_status = true;
-  let enable_copy_to_clipbaord = true;
-  let copied_text_status = false;
 
   let added_translations_indexes: number[] = [];
   let edited_translations_indexes = new Set<number>();
@@ -108,8 +100,6 @@
       })
     );
   });
-
-  $: copied_text_status && setTimeout(() => (copied_text_status = false), 1400);
 
   const save_data = createMutation({
     mutationKey: ['sarga', 'save_edited_data'],
@@ -261,14 +251,6 @@
       <Icon src={BiHelpCircle} class="mt-1 text-3xl text-sky-500 dark:text-sky-400" />
     </button>
   {/if}
-  {#if copied_text_status}
-    <span
-      class="fixed bottom-2 right-2 z-50 cursor-default select-none font-bold text-green-700 dark:text-green-300"
-    >
-      <Icon src={BsClipboard2Check} />
-      Copied to Clipboard
-    </span>
-  {/if}
 </div>
 {#if $editing_status_on}
   <button
@@ -303,7 +285,6 @@
   {#if !$sarga_data.isFetching}
     <div transition:fade={{ duration: 250 }} class="space-y-[0.15rem]">
       {#each transliterated_sarga_data as shloka_lines, i (i)}
-        {@const line_split = shloka_lines.split('\n')}
         <!-- with 0 and -1 index -->
         {@const trans_index = transliterated_sarga_data.length - 1 === i ? -1 : i}
         <div class="rounded-lg px-2 py-1 hover:bg-gray-200 dark:hover:bg-gray-800">
@@ -316,98 +297,21 @@
               </div>
             {/if}
             <div class="mt-0 w-full space-y-1">
-              <!-- svelte-ignore a11y-no-static-element-interactions -->
-              <div
-                on:dblclick={() => {
-                  if (!enable_copy_to_clipbaord) return;
-                  copy_text_to_clipboard(shloka_lines);
-                  copied_text_status = true;
+              <TextDisplay
+                {...{
+                  added_translations_indexes,
+                  edit_language_typer_status,
+                  edited_translations_indexes,
+                  editing_status_on,
+                  shloka_lines,
+                  trans_en_data,
+                  trans_index,
+                  trans_lang_data,
+                  trans_lang_data_query_key,
+                  trans_lang,
+                  sanskrit_mode
                 }}
-              >
-                {#each line_split as line_shlk}
-                  <!-- if needed add 'whitespace-pre-wrap'2 -->
-                  <div class="font">{line_shlk}</div>
-                {/each}
-              </div>
-              {#if $trans_en_data.isSuccess && $trans_en_data.data.size !== 0}
-                <!-- svelte-ignore a11y-no-static-element-interactions -->
-                <div
-                  on:dblclick={() => {
-                    if (!enable_copy_to_clipbaord) return;
-                    copy_text_to_clipboard(
-                      get_possibily_not_undefined($trans_en_data.data.get(trans_index))
-                    );
-                    copied_text_status = true;
-                  }}
-                  class="text-stone-500 dark:text-slate-400"
-                >
-                  {#if $trans_en_data.data.has(trans_index)}
-                    <!-- Usually translations are single but still... -->
-                    {#each get_possibily_not_undefined($trans_en_data.data.get(trans_index)).split('\n') as line_trans}
-                      <div>{line_trans}</div>
-                    {/each}
-                  {/if}
-                </div>
-              {/if}
-              {#if $editing_status_on && $trans_lang_data.isSuccess}
-                <div transition:slide>
-                  {#if !$trans_lang_data.data?.has(trans_index)}
-                    <button
-                      on:click={async () => {
-                        await update_trans_data(trans_index, '');
-                        added_translations_indexes.push(trans_index);
-                      }}
-                      class="btn m-0 rounded-md bg-surface-500 p-0 px-1 font-bold text-white dark:bg-surface-500"
-                    >
-                      <Icon src={RiSystemAddLargeLine} />
-                    </button>
-                  {:else}
-                    <textarea
-                      on:input={(e) => {
-                        if (!added_translations_indexes.includes(trans_index))
-                          edited_translations_indexes.add(trans_index);
-                        if (edit_language_typer_status)
-                          LipiLekhikA.mukhya(
-                            e.target,
-                            // @ts-ignore
-                            e.data,
-                            $trans_lang,
-                            true,
-                            // @ts-ignore
-                            (val) => {
-                              update_trans_data(trans_index, val);
-                            },
-                            sanskrit_mode
-                          );
-                        else {
-                          update_trans_data(trans_index, e.currentTarget.value);
-                        }
-                      }}
-                      class="font textarea h-16 w-full"
-                      value={$trans_lang_data.data?.get(trans_index)}
-                    ></textarea>
-                  {/if}
-                </div>
-              {:else if $trans_lang_data.isSuccess && $trans_lang_data.data.size !== 0}
-                <!-- svelte-ignore a11y-no-static-element-interactions -->
-                <div
-                  on:dblclick={() => {
-                    if (!enable_copy_to_clipbaord) return;
-                    copy_text_to_clipboard(
-                      get_possibily_not_undefined($trans_lang_data.data?.get(trans_index))
-                    );
-                    copied_text_status = true;
-                  }}
-                  class="text-yellow-700 dark:text-yellow-500"
-                >
-                  {#if $trans_lang_data.data?.has(trans_index)}
-                    <!-- Usually translations are single but still... -->
-                    {#each get_possibily_not_undefined($trans_lang_data.data?.get(trans_index)).split('\n') as line_trans}
-                      <div class="font">{line_trans}</div>
-                    {/each}
-                  {/if}
-                </div>
-              {/if}
+              />
             </div>
           </div>
         </div>
@@ -415,9 +319,3 @@
     </div>
   {/if}
 </div>
-
-<style>
-  .font {
-    font-family: 'Nirmala UI', sans-serif;
-  }
-</style>
