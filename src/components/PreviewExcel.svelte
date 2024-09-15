a3044a4ab05bc54f0fac40ba218ebcce71842e4a
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-09-15 12:41:46 +0530

    added overflow behaviour

--- src/components/PreviewExcel.svelte
+++ src/components/PreviewExcel.svelte
@@ -21,53 +21,67 @@
     );
     return lang || '';
   };
+
+  let overflow_behavior = 'hidden';
 </script>
 
-<Modal
-  modal_open={file_preview_opened}
-  close_on_click_outside={false}
-  class="rounded-lg border-2 border-blue-700 bg-[aliceblue] dark:border-blue-500 dark:bg-slate-800"
->
-  <div class="ml-4 flex">
-    <span class="text-2xl">
-      <a href={file_link} class="ml-2" download={file_name}>
-        <Icon
-          src={RiSystemDownloadLine}
-          class="text-blue-800 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-500"
-        />
-      </a>
-    </span>
-  </div>
-  <TabGroup>
-    {#each workbook.worksheets as worksheet, i}
-      <Tab bind:group={sheet_number} name={worksheet.name} value={i}>
-        <span class="font-bold">{worksheet.name}</span>
-      </Tab>
-    {/each}
-    <div slot="panel" class="overflow-scroll">
-      {@const worksheet = workbook.worksheets[sheet_number]}
-      <table class="table table-hover table-compact table-cell-fit">
-        <thead>
-          <tr>
-            {#each Array(worksheet.columnCount) as _, i}
-              {@const row_value = worksheet.getCell(1, i + 1).value}
-              <th class="text-center">{row_value ?? ''}</th>
-            {/each}
-          </tr>
-        </thead>
-        <tbody>
-          {#each Array(worksheet.rowCount) as _, row_i}
+<div>
+  <Modal
+    modal_open={file_preview_opened}
+    close_on_click_outside={false}
+    class="rounded-lg border-2 border-blue-700 bg-[aliceblue] dark:border-blue-500 dark:bg-slate-800"
+  >
+    <div class="ml-4 flex select-none space-x-4">
+      <span class="text-2xl">
+        <a href={file_link} class="ml-2" download={file_name}>
+          <Icon
+            src={RiSystemDownloadLine}
+            class="text-blue-800 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-500"
+          />
+        </a>
+      </span>
+      <label class="space-x-1">
+        <span>Overflow</span>
+        <select class="select w-24 p-1" bind:value={overflow_behavior}>
+          <option value="hidden">Hidden</option>
+          <option value="scroll">Scroll</option>
+        </select>
+      </label>
+    </div>
+    <TabGroup>
+      {#each workbook.worksheets as worksheet, i}
+        <Tab bind:group={sheet_number} name={worksheet.name} value={i}>
+          <span class="font-bold">{worksheet.name}</span>
+        </Tab>
+      {/each}
+      <div slot="panel" class="overflow-scroll">
+        {@const worksheet = workbook.worksheets[sheet_number]}
+        <table class="table table-hover table-compact table-cell-fit">
+          <thead>
             <tr>
-              {#each Array(worksheet.columnCount) as _, column_i}
-                {@const row_value =
-                  worksheet.getCell(row_i + 2, column_i + 1).value?.toLocaleString() ?? ''}
-                {@const lang = get_lang_code_of_columnn(worksheet, column_i)}
-                <td><pre class={get_text_font(lang)}>{row_value}</pre></td>
+              {#each Array(worksheet.columnCount) as _, i}
+                {@const row_value = worksheet.getCell(1, i + 1).value}
+                <th class="text-center">{row_value ?? ''}</th>
               {/each}
             </tr>
-          {/each}
-        </tbody>
-      </table>
-    </div>
-  </TabGroup>
-</Modal>
+          </thead>
+          <tbody>
+            {#each Array(worksheet.rowCount) as _, row_i}
+              <tr>
+                {#each Array(worksheet.columnCount) as _, column_i}
+                  {@const row_value =
+                    worksheet.getCell(row_i + 2, column_i + 1).value?.toLocaleString() ?? ''}
+                  {@const lang = get_lang_code_of_columnn(worksheet, column_i)}
+                  <td>
+                    <pre
+                      class={`${get_text_font(lang)} hide-scrollbar max-w-72 scroll-m-0 ${overflow_behavior === 'scroll' ? 'overflow-scroll' : 'overflow-hidden'} text-sm`}>{row_value}</pre>
+                  </td>
+                {/each}
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+      </div>
+    </TabGroup>
+  </Modal>
+</div>
