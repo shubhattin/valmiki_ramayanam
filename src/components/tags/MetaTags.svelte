5e96ad73424e7d433859dac5314c694cd5ef5e75
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-09-15 17:57:43 +0530

    updated project banner url

--- src/components/tags/MetaTags.svelte
+++ src/components/tags/MetaTags.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+  import ProjectBannerURL from '../../../static/img/Project_Banner.webp';
+
   export let title: string;
   export let description: string | null = null;
 </script>
 
 <svelte:head>
-  <meta property="og:image" content="/static/img/Project_Banner.webp" />
+  <meta property="og:image" content={ProjectBannerURL} />
   <meta property="og:image:width" content="501.4" />
   <meta property="og:image:height" content="229.32" />
   <title>{title}</title>
--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   },
   server: {
     fs: {
-      allow: ['./data/ramayan']
+      allow: ['./data/ramayan', './static/img']
     }
   }
 });
