1a0039fa2e4a9f7d6990fecb8c79e88d1bedeca7
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-09-15 17:49:59 +0530

    added image mete tags

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # श्रीमद्वाल्मीकीयरामायणम् (Ṣrīmadvālmīkīya*rāmāyaṇam*)
 
-![Ramayanam](./others/image_files//Project%20Banner.webp)
+![Ramayanam](./static/img/Project_Banner.webp)
 
 **[श्रीमद्रामायणस्य पठनम्](https://ramayanam.thesanskritchannel.org/)**
 
--- src/components/tags/MetaTags.svelte
+++ src/components/tags/MetaTags.svelte
@@ -4,6 +4,9 @@
 </script>
 
 <svelte:head>
+  <meta property="og:image" content="/static/img/Project_Banner.webp" />
+  <meta property="og:image:width" content="501.4" />
+  <meta property="og:image:height" content="229.32" />
   <title>{title}</title>
   <meta property="og:title" content={title} />
   <meta property="og:site_name" content={title} />
