1fee2ffc687487f792afcf7bbf9aee7c73889496
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-09-18 07:22:26 +0530

    refactored download login into another component

--- src/components/pages/main_page/image_tool/ImageUtility.svelte
+++ src/components/pages/main_page/image_tool/ImageUtility.svelte
@@ -6,31 +6,24 @@
     image_sarga,
     image_script,
     image_shloka,
-    scaling_factor,
     canvas,
     image_sarga_data,
     image_trans_data,
-    set_background_image_type,
     shaded_background_image_status,
-    background_image,
-    IMAGE_DIMENSIONS,
     get_units,
     shloka_texts,
     trans_text
   } from './state';
   import { viewing_script, BASE_SCRIPT } from '@state/main_page/main_state';
-  import { LANG_LIST, SCRIPT_LIST } from '@tools/lang_list';
+  import { LANG_LIST } from '@tools/lang_list';
   import { FONT_NAMES, get_text_font, load_font } from '@tools/font_tools';
   import Icon from '@tools/Icon.svelte';
   import { TiArrowBackOutline, TiArrowForwardOutline } from 'svelte-icons-pack/ti';
   import { LanguageIcon } from '@components/icons';
-  import { download_file_in_browser } from '@tools/download_file_browser';
-  import { BsDownload } from 'svelte-icons-pack/bs';
   import { SlideToggle, popup } from '@skeletonlabs/skeleton';
-  import JSZip from 'jszip';
   import * as fabric from 'fabric';
   import { lipi_parivartak_async } from '@tools/converter';
-  import { dataURLToBlob } from '@tools/kry';
+  import ImageDownloader from './ImageDownloader.svelte';
 
   export let mounted: boolean;
 
@@ -124,97 +117,6 @@
     (async () => {
       await render_all_texts($image_shloka, $image_script);
     })();
-
-  const remove_background_image = async () => {
-    $canvas.getObjects().forEach((obj) => {
-      if (obj.type === 'image') $canvas.remove(obj);
-    });
-    $canvas.requestRenderAll();
-  };
-  const add_background_image = async () => {
-    $canvas.add($background_image);
-    $canvas.sendObjectToBack($background_image);
-    $canvas.requestRenderAll();
-  };
-  const download_image_as_png = async (
-    remove_background: boolean,
-    download = true,
-    shloka_num: number | null = null,
-    restore = true
-  ) => {
-    if (remove_background) await remove_background_image();
-    else if ($shaded_background_image_status) await set_background_image_type(false);
-
-    const url = $canvas.toDataURL({
-      format: 'png',
-      multiplier: 1 / $scaling_factor
-    });
-    const name = `${$image_kANDa}-${$image_sarga} Shloka No. ${shloka_num ?? $image_shloka}${remove_background ? '' : ' (with background)'}.png`;
-    if (download) download_file_in_browser(url, name);
-    if (remove_background) await add_background_image();
-    else if ($shaded_background_image_status && restore)
-      await set_background_image_type($shaded_background_image_status);
-    return {
-      url,
-      name
-    };
-  };
-  const download_image_as_svg = async (download = true, shloka_num: number | null = null) => {
-    await remove_background_image();
-    const svg_text = $canvas.toSVG({
-      width: `${IMAGE_DIMENSIONS[0]}`,
-      height: `${IMAGE_DIMENSIONS[1]}`,
-      viewBox: {
-        x: 0,
-        y: 0,
-        width: get_units(IMAGE_DIMENSIONS[0]),
-        height: get_units(IMAGE_DIMENSIONS[1])
-      }
-    });
-    const blob = new Blob([svg_text], { type: 'image/svg+xml' });
-    const name = `${$image_kANDa}-${$image_sarga} Shloka No. ${shloka_num ?? $image_shloka}.svg`;
-    if (download) {
-      const svg_url = URL.createObjectURL(blob);
-      download_file_in_browser(svg_url, name);
-    }
-    add_background_image();
-    return {
-      blob,
-      name
-    };
-  };
-
-  const download_png_zip = async (remove_back: boolean) => {
-    const zip = new JSZip();
-    for (let i = -1; i <= shloka_count; i++) {
-      await render_all_texts(i, $image_script);
-      const { url, name } = await download_image_as_png(remove_back, false, i, false);
-      const blob = dataURLToBlob(url);
-      zip.file(name, blob);
-    }
-    await render_all_texts($image_shloka, $image_script);
-    const zip_blob = await zip.generateAsync({ type: 'blob' });
-    download_file_in_browser(
-      URL.createObjectURL(zip_blob),
-      `${$image_kANDa}-${$image_sarga} PNG files${remove_back ? '' : ' (with background)'}.zip`
-    );
-    // ^ restore the original state
-  };
-  const download_svg_zip = async () => {
-    const zip = new JSZip();
-    for (let i = -1; i <= shloka_count; i++) {
-      await render_all_texts(i, $image_script);
-      const { blob, name } = await download_image_as_svg(false, i);
-      zip.file(name, blob);
-    }
-    await render_all_texts($image_shloka, $image_script);
-    const zip_blob = await zip.generateAsync({ type: 'blob' });
-    download_file_in_browser(
-      URL.createObjectURL(zip_blob),
-      `${$image_kANDa}-${$image_sarga} SVG files.zip`
-    );
-    // ^ restore the original state
-  };
 </script>
 
 <div class="flex space-x-2 text-sm">
@@ -263,59 +165,7 @@
       {/each}
     </select>
   </label>
-  <button
-    use:popup={{
-      event: 'click',
-      target: 'image_download',
-      placement: 'bottom'
-    }}
-    on:dblclick={() => download_image_as_png(true)}
-    class="btn inline-flex rounded-lg p-1 text-sm"
-  >
-    <Icon src={BsDownload} class="-mt-1 mr-1 text-2xl" />
-  </button>
-  <div class="card z-50 space-y-0 rounded-md p-1 shadow-xl" data-popup="image_download">
-    <div class="flex items-center justify-center space-x-2">
-      <button
-        on:click={() => download_image_as_svg()}
-        class="btn rounded-md p-1 text-sm hover:bg-gray-200 dark:hover:bg-gray-700"
-      >
-        SVG
-      </button>
-      <button
-        on:click={() => download_image_as_png(true)}
-        class="btn rounded-md p-1 text-sm hover:bg-gray-200 dark:hover:bg-gray-700"
-      >
-        PNG
-      </button>
-      <button
-        on:click={() => download_image_as_png(false)}
-        class="btn rounded-md p-1 text-xs hover:bg-gray-200 dark:hover:bg-gray-700"
-      >
-        PNG (with background)
-      </button>
-    </div>
-    <div class="flex items-center justify-center space-x-2">
-      <button
-        on:click={() => download_svg_zip()}
-        class="btn rounded-md p-1 text-sm hover:bg-gray-200 dark:hover:bg-gray-700"
-      >
-        SVG Zip
-      </button>
-      <button
-        on:click={() => download_png_zip(true)}
-        class="btn rounded-md p-1 text-sm hover:bg-gray-200 dark:hover:bg-gray-700"
-      >
-        PNG Zip
-      </button>
-      <button
-        on:click={() => download_png_zip(false)}
-        class=" btn rounded-md p-1 text-xs hover:bg-gray-200 dark:hover:bg-gray-700"
-      >
-        PNG Zip (with background)
-      </button>
-    </div>
-  </div>
+  <ImageDownloader {render_all_texts} />
   <span class="inline-flex flex-col">
     <SlideToggle
       name="from_text_type"
