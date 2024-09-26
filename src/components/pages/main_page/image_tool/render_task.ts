0de92297690c2836ceaae3f9b9f96d62c890660f
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-09-26 20:01:03 +0530

    update

--- src/components/pages/main_page/image_tool/render_task.ts
+++ src/components/pages/main_page/image_tool/render_task.ts
@@ -6,6 +6,7 @@ const render_text_args_schema = z.object({
   text: z.string(),
   font_url: z.string(),
   font_size: z.number(),
+  font_scale: z.number(),
   color: z.string(),
   line_index: z.number().optional(),
   text_type: z.union([z.literal('main'), z.literal('normal')]).optional(),
@@ -58,6 +59,7 @@ export const render_text = async (opts: z.infer<typeof render_text_args_schema>)
   const left = get_units(opts.left);
   const top = get_units(opts.top ?? 0);
   const bottom = get_units(opts.bottom ?? 0);
+  let font_scale = opts.font_scale;
 
   const WIDTH_ACTUAL = right - left;
   const WIDTH = WIDTH_ACTUAL * width_usage_factor;
@@ -82,8 +84,10 @@ export const render_text = async (opts: z.infer<typeof render_text_args_schema>)
     }
     text_used += word + (i === words.length - 1 ? '' : ' ');
   }
-  let text_path_scale = get_font_size_for_path(font_size);
-  let text_group = new fabric.Group([], {
+
+  const FONT_SCALE_STEP = 0.05;
+  let text_path_scale: number = 0;
+  const text_group = new fabric.Group([], {
     lockRotation: true,
     lockMovementY: lockMovementY ?? true,
     lockMovementX: lockMovementX ?? true,
@@ -93,6 +97,7 @@ export const render_text = async (opts: z.infer<typeof render_text_args_schema>)
   const lines = text_used.split('\n');
   let prev_height = 0;
   const NEW_LINE_SPACING = font_size;
+  // ^ currenyly works fine for our case
 
   const render_line = async (line: string) => {
     const text_path = new fabric.Path(await get_text_svg_path(line, font_url), {
@@ -128,28 +133,45 @@ export const render_text = async (opts: z.infer<typeof render_text_args_schema>)
 
   for (let i = 0; i < lines.length; i++) {
     const line = lines[i];
+    text_path_scale = get_font_size_for_path(font_size * font_scale);
     if (!multi_line_text) await render_line(line);
     else {
       const words = line.split(' ');
-      let allowed_words: string[] = [];
-      for (let word_i = 0; word_i < words.length; word_i++) {
-        const word = words[word_i];
-        let current_word = allowed_words.join(' ') + (allowed_words.length !== 0 ? ' ' : '') + word;
-        const text_path = new fabric.Path(await get_text_svg_path(current_word, font_url), {
-          fill: color
-        });
-        let height = text_path.height * text_path_scale;
-        let width = text_path.width * text_path_scale;
-        if (WIDTH > width) allowed_words.push(word);
-        else {
-          await render_line(allowed_words.join(' '));
-          // new line should be started as the current word is not fitting
-          allowed_words = [word];
+      const render_multiple_line = async () => {
+        let allowed_words: string[] = [];
+        for (let word_i = 0; word_i < words.length; word_i++) {
+          const word = words[word_i];
+          let current_word =
+            allowed_words.join(' ') + (allowed_words.length !== 0 ? ' ' : '') + word;
+          const text_path = new fabric.Path(await get_text_svg_path(current_word, font_url), {
+            fill: color
+          });
+          let height = text_path.height * text_path_scale;
+          let width = text_path.width * text_path_scale;
+          if (WIDTH > width) allowed_words.push(word);
+          else {
+            await render_line(allowed_words.join(' '));
+            // new line should be started as the current word is not fitting
+            allowed_words = [word];
+          }
+        }
+        // last group of allowed words will rendered after done here
+        if (allowed_words.length !== 0) await render_line(allowed_words.join(' '));
+      };
+      for (let iter = 0; true; iter++) {
+        await render_multiple_line();
+        if (HEIGHT / text_group.height >= 1) {
+          console.log('correct', text_path_scale, font_scale);
+          break;
+        } else {
+          console.log('height exceeded', HEIGHT / text_group.height, text_path_scale);
+          text_path_scale = get_font_size_for_path(
+            font_size * (font_scale - (iter + 1) * FONT_SCALE_STEP)
+          );
+          console.log('new scale', text_path_scale);
+          text_group.removeAll();
         }
       }
-      // last group of allowed words will rendered after done here
-      if (allowed_words.length !== 0) await render_line(allowed_words.join(' '));
-      console.log(HEIGHT / text_group.height);
     }
   }
   return text_group;
