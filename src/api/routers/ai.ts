515cc1031ac39acdacf26c697c27437b2e691c7c
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-10-02 07:53:19 +0530

    using open ai lib in edge

--- src/api/routers/ai.ts
+++ src/api/routers/ai.ts
@@ -2,9 +2,8 @@ import { protectedAdminProcedure, t } from '@api/trpc_init';
 import { createOpenAI } from '@ai-sdk/openai';
 import { generateObject } from 'ai';
 import { env } from '$env/dynamic/private';
-import type OpenAI from 'openai';
+import OpenAI from 'openai';
 import { z } from 'zod';
-import { fetch_post } from '@tools/fetch';
 import { ai_sample_data } from './ai_sample_data/sample_data';
 import { delay } from '@tools/delay';
 
@@ -38,6 +37,9 @@ const get_image_prompt_router = protectedAdminProcedure
     return result.object;
   });
 
+const openai = new OpenAI({
+  apiKey: env.OPENAI_API_KEY
+});
 const get_generated_images_router = protectedAdminProcedure
   .input(
     z.object({
@@ -47,34 +49,19 @@ const get_generated_images_router = protectedAdminProcedure
     })
   )
   .mutation(async ({ input: { image_prompt, number_of_images, use_sample_data } }) => {
+    openai.images.generate({
+      prompt: 'kjk',
+      model: 'dall-e-3'
+    });
     const get_single_image = async () => {
-      const req = await fetch_post('https://api.openai.com/v1/images/generations', {
-        json: {
-          model: 'dall-e-3',
-          prompt: image_prompt,
-          n: 1,
-          size: '1024x1024',
-          quality: 'standard',
-          response_format: 'b64_json'
-        },
-        headers: {
-          Authorization: `Bearer ${env.OPENAI_API_KEY}`
-        }
+      const resp = await openai.images.generate({
+        model: 'dall-e-3',
+        prompt: image_prompt,
+        n: 1,
+        size: '1024x1024',
+        quality: 'standard',
+        response_format: 'b64_json'
       });
-      const raw_resp = await req.json();
-      // console.log(raw_resp);
-      const resp = z
-        .object({
-          created: z.number().int(),
-          data: z
-            .object({
-              revised_prompt: z.string(),
-              b64_json: z.string()
-            })
-            .array()
-        })
-        .parse(raw_resp);
-
       return {
         created: resp.created,
         revised_prompt: resp.data[0].revised_prompt,
