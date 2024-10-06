740c0b7efbfefb54be46cfa7a492d5e0e9fe2f94
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-10-06 12:40:41 +0530

    loading local config via yaml

--- src/components/pages/main_page/load_local_config.ts
+++ src/components/pages/main_page/load_local_config.ts
@@ -2,6 +2,8 @@ import { z } from 'zod';
 import { env } from '$env/dynamic/public';
 
 export async function loadLocalConfig() {
+  const yaml = await import('js-yaml');
+
   const config_schema = z.object({
     image_tool_opened: z.boolean().optional(),
     view_translation_status: z.boolean().optional(),
@@ -13,11 +15,11 @@ export async function loadLocalConfig() {
   let config: z.infer<typeof config_schema> = {};
   if (env.PUBLIC_LOCAL_CONFIG === 'true') {
     try {
-      const url = new URL('/src/components/pages/main_page/local_config.json', import.meta.url)
+      const url = new URL('/src/components/pages/main_page/local_config.yaml', import.meta.url)
         .href;
       const request = await fetch(url);
-      const data = await request.json();
-      config = config_schema.parse(data);
+      const data = await request.text();
+      config = config_schema.parse(yaml.load(data));
     } catch (e) {}
   }
 
