c95d2cb395a10178cb55d10bf03fe254014823ae
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-11-08 14:40:28 +0530

    init changes

--- src/components/pages/main_page/display/tests/bulk_trans_edit.test.ts
+++ src/components/pages/main_page/display/tests/bulk_trans_edit.test.ts
@@ -1,11 +1,12 @@
 import { describe, expect, it } from 'vitest';
-import { trans_map_to_text } from '../trans_bulk_funcs';
+import { text_to_trans_map, trans_map_to_text } from '../trans_bulk_funcs';
 
 const text = `0. Shloka Starts
 
 
 1. Shloka 1
 Contents of Shloka 1
+Good Text
 
 
 2. Shloka 2
@@ -75,7 +76,7 @@ Contents of Shloka 16
 
 const map = new Map<number, string>();
 map.set(0, 'Shloka Starts');
-map.set(1, 'Shloka 1\nContents of Shloka 1');
+map.set(1, 'Shloka 1\nContents of Shloka 1\nGood Text');
 map.set(2, 'Shloka 2\nContents of Shloka 2');
 map.set(3, 'Shloka 3\nContents of Shloka 3');
 map.set(4, 'Shloka 4\nContents of Shloka 4');
@@ -94,8 +95,85 @@ map.set(16, 'Shloka 16\nContents of Shloka 16');
 map.set(17, '---------------');
 map.set(-1, 'Shloka Ends');
 
+const text1 = `0. Shloka Starts
+
+
+1. Shloka 1
+Contents of Shloka 1
+Good Text
+
+
+2. Shloka 2
+Contents of Shloka 2
+
+
+3. Shloka 3
+Contents of Shloka 3
+
+
+4. Shloka 4
+Contents of Shloka 4
+
+
+5. Shloka 5
+Contents of Shloka 5
+
+
+6. Shloka 6
+Contents of Shloka 6
+
+
+7. Shloka 7
+Contents of Shloka 7
+
+
+8. Shloka 8
+Contents of Shloka 8
+
+
+9. Shloka 9
+Contents of Shloka 9
+
+
+10. Shloka 10
+Contents of Shloka 10
+
+
+11. Shloka 11
+Contents of Shloka 11
+
+
+12. Shloka 12
+Contents of Shloka 12
+
+
+13. Shloka 13
+Contents of Shloka 13
+
+
+14. Shloka 14
+Contents of Shloka 14
+
+
+15. Shloka 15
+Contents of Shloka 15
+
+
+16. Shloka 16
+Contents of Shloka 16
+
+
+17. ---------------
+
+
+-1. Shloka Ends`;
+
 describe('Testing Bulk Edit Translations', () => {
   it('Translation Map to Text', async () => {
     expect(trans_map_to_text(map, 17)).toBe(text);
+  });
+  it('Text to Translation Map', async () => {
+    const out_map = text_to_trans_map(text1, 17);
+    map.forEach((value, i) => expect(value).toBe(out_map.get(i)));
   });
 });
--- src/components/pages/main_page/display/trans_bulk_funcs.ts
+++ src/components/pages/main_page/display/trans_bulk_funcs.ts
@@ -1,6 +1,8 @@
+const NON_EXIST_INDICATOR = '---------------';
+const EXTRA_NEW_LINE_REGEX = /(?<=\n\n\n)\n+/gm;
+
 export function trans_map_to_text(trans_map: Map<number, string>, shloka_count: number) {
   let texts_per_shloka: string[] = [];
-  const NON_EXIST_INDICATOR = '---------------';
   const shloka_template_func = (text: string, num: number) => `${num}. ${text}`;
 
   const check_func = (i: number) => {
@@ -10,4 +12,17 @@ export function trans_map_to_text(trans_map: Map<number, string>, shloka_count: 
   for (let i = 0; i <= shloka_count; i++) check_func(i);
   check_func(-1);
   return texts_per_shloka.join('\n\n\n');
+}
+
+/**
+ * Rules for text
+ * - Each shloka should be separated by two blank lines(`\n\n\n`)
+ * - Shloka number can be indicated in either `{num}. {text}` or `{num}- {text}` format
+ * - If shloka number is not there then it will be interpreted in its usual order
+ */
+export function text_to_trans_map(text: string, shloka_count: number) {
+  text = text.replaceAll(EXTRA_NEW_LINE_REGEX, '\n\n\n');
+  const texts = text.split('\n\n\n');
+  const trans_map = new Map<number, string>();
+  return trans_map;
 }
