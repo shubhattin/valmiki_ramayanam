dabddc550e2da4b7729485d3e7108d119a089e3f
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-11-11 20:38:41 +0530

    init

--- src/api/routers/ai/get_image_prompt.ts
+++ src/api/routers/ai/get_image_prompt.ts
@@ -1,33 +1,12 @@
-import { protectedAdminProcedure, t } from '~/api/trpc_init';
-import { generateObject } from 'ai';
-import { z } from 'zod';
-import { createOpenAI } from '@ai-sdk/openai';
-import { env } from '$env/dynamic/private';
-
-const openai_text_model = createOpenAI({ apiKey: env.OPENAI_API_KEY });
+import { protectedAdminProcedure } from '~/api/trpc_init';
+import { tasks } from '@trigger.dev/sdk/v3';
+import { get_image_prompt_schema } from './ai_types';
 
 export const get_image_prompt_route = protectedAdminProcedure
-  .input(
-    z.object({
-      messages: z
-        .object({
-          role: z.union([z.literal('user'), z.literal('assistant')]),
-          content: z.string()
-        })
-        .array()
-    })
-  )
+  .input(get_image_prompt_schema.input)
   .mutation(async ({ input: { messages } }) => {
-    try {
-      const result = await generateObject({
-        model: openai_text_model('gpt-4o'),
-        messages,
-        schema: z.object({
-          image_prompt: z.string()
-        })
-      });
-      return result.object;
-    } catch (e) {
-      return { image_prompt: null };
-    }
+    const handle = await tasks.trigger('ai_get_image_prompt', {
+      messages
+    });
+    return handle;
   });
