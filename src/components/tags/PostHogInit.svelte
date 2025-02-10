41b5286b41fd86d5ac6377718b668336d6465e9e
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-02-10 20:30:25 +0530

    undo

--- src/components/tags/PostHogInit.svelte
+++ src/components/tags/PostHogInit.svelte
@@ -3,10 +3,15 @@
   import { browser } from '$app/environment';
 
   onMount(() => {
-    if (browser && import.meta.env.PROD && import.meta.env.VITE_POSTHOG_KEY) {
+    if (
+      browser &&
+      import.meta.env.PROD &&
+      import.meta.env.VITE_POSTHOG_KEY &&
+      (import.meta.env.VITE_POSTHOG_URL || import.meta.env.VITE_SITE_URL)
+    ) {
       import('posthog-js').then((posthog) => {
         posthog.default.init(import.meta.env.VITE_POSTHOG_KEY, {
-          api_host: `${import.meta.env.VITE_SITE_URL}/ingest`,
+          api_host: `${import.meta.env.VITE_POSTHOG_URL ?? import.meta.env.VITE_SITE_URL}/ingest`,
           person_profiles: 'identified_only',
           ui_host: 'https://us.posthog.com'
         });
