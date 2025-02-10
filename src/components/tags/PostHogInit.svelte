03242c8e22b768ee732da98aed48bb6a6e620eb4
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-02-10 16:25:55 +0530

    update

--- src/components/tags/PostHogInit.svelte
+++ src/components/tags/PostHogInit.svelte
@@ -4,14 +4,14 @@
 
   onMount(() => {
     if (
+      browser &&
       import.meta.env.PROD &&
-      import.meta.env.VITE_POSTHOG_ID &&
-      import.meta.env.VITE_SITE_URL &&
-      browser
+      import.meta.env.VITE_POSTHOG_KEY &&
+      import.meta.env.VITE_POSTHOG_URL
     ) {
       import('posthog-js').then((posthog) => {
-        posthog.default.init(import.meta.env.VITE_POSTHOG_ID, {
-          api_host: `${import.meta.env.VITE_SITE_URL}/ingest`,
+        posthog.default.init(import.meta.env.VITE_POSTHOG_KEY, {
+          api_host: `${import.meta.env.VITE_POSTHOG_URL}/ingest`,
           person_profiles: 'identified_only',
           ui_host: 'https://us.posthog.com'
         });
