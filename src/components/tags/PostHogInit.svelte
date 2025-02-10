4dfb96a752fa83a59ab895a721a1c3ba335eccc6
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-02-10 20:25:50 +0530

    try fix

--- src/components/tags/PostHogInit.svelte
+++ src/components/tags/PostHogInit.svelte
@@ -3,15 +3,10 @@
   import { browser } from '$app/environment';
 
   onMount(() => {
-    if (
-      browser &&
-      import.meta.env.PROD &&
-      import.meta.env.VITE_POSTHOG_KEY &&
-      (import.meta.env.VITE_POSTHOG_URL || import.meta.env.VITE_SITE_URL)
-    ) {
+    if (browser && import.meta.env.PROD && import.meta.env.VITE_POSTHOG_KEY) {
       import('posthog-js').then((posthog) => {
         posthog.default.init(import.meta.env.VITE_POSTHOG_KEY, {
-          api_host: `${import.meta.env.VITE_POSTHOG_URL ?? import.meta.env.VITE_SITE_URL}/ingest`,
+          api_host: `${import.meta.env.VITE_SITE_URL}/ingest`,
           person_profiles: 'identified_only',
           ui_host: 'https://us.posthog.com'
         });
