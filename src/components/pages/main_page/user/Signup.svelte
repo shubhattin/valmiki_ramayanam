2f3c4dad5b8911e5b819dde2f4cc9efe1fa760a4
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-03-07 09:31:06 +0530

    add captcha

--- src/components/pages/main_page/user/Signup.svelte
+++ src/components/pages/main_page/user/Signup.svelte
@@ -2,17 +2,37 @@
   import { signIn } from '~/lib/auth-client';
   import Icon from '~/tools/Icon.svelte';
   import { GoogleIcon } from '~/components/icons';
+  import { PUBLIC_TURNSTILE_SITE_KEY } from '$env/static/public';
+  import { Turnstile } from 'svelte-turnstile';
+
+  let callback = $state<{ token: string; preClearanceObtained: boolean } | null>(null);
 </script>
 
-<div class="mt-4 flex items-center justify-center">
+<div class="mt-4 flex flex-col items-center justify-center">
   <button
+    disabled={!callback}
     onclick={async () => {
       await signIn.social({
         provider: 'google',
-        callbackURL: window.location.href
+        callbackURL: window.location.href,
+        fetchOptions: {
+          headers: {
+            'x-captcha-response': callback?.token!
+          }
+        }
       });
     }}
-    class="variant-outline-secondary btn flex gap-2 rounded-lg font-semibold"
+    class="variant-outline-primary btn flex gap-2 rounded-lg font-semibold"
     ><Icon src={GoogleIcon} class="inline-block text-[1.25rem]" />Signup with Google</button
   >
+  <div class="mt-4">
+    <Turnstile
+      siteKey={PUBLIC_TURNSTILE_SITE_KEY}
+      on:callback={async (event) => {
+        if (event.detail) {
+          callback = event.detail;
+        }
+      }}
+    />
+  </div>
 </div>
